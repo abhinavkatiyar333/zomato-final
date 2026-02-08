90d16e1ec1387ae36b86410a4e06b9b6d4277cc8
abhinavkatiyar333 <abhinavkatiyar727@gmail.com>
2026-02-08 13:23:12 +0530

    Fix Mongo env variable

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
   }
